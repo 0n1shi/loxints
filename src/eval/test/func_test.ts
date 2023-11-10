29f3ede21a63543ae77bce3552a6247ae5256327
Kazuki Onishi <princeontrojanhorse@gmail.com>
2023-11-11 00:14:05 +0900

    testing eval

--- src/eval/test/func_test.ts
+++ src/eval/test/func_test.ts
@@ -1,11 +1,21 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/mod.ts";
-import { evaluatePrimary } from "../func.ts";
+import { evaluateCall, evaluatePrimary } from "../func.ts";
 import { primaryTests } from "../../test/data/primary.ts";
+import { callTests } from "../../test/data/call.ts";
 
-Deno.test("Testing ast for function declarations", async (context) => {
+Deno.test("Testing evaluation of primary", async (context) => {
   for (const test of primaryTests) {
     await context.step(test.name, () => {
       const value = evaluatePrimary(test.ast, test.environmentBefore);
+      assertEquals(value, test.value);
+      assertEquals(test.environmentBefore, test.environmentAfter);
+    });
+  }
+});
+Deno.test("Testing evaluation of call", async (context) => {
+  for (const test of callTests) {
+    await context.step(test.name, () => {
+      const value = evaluateCall(test.ast, test.environmentBefore);
       assertEquals(value, test.value);
       assertEquals(test.environmentBefore, test.environmentAfter);
     });
--- src/test/data/call.ts
+++ src/test/data/call.ts
@@ -1,17 +1,19 @@
 import { Token, TokenType } from "../../token/type.ts";
 import {
   Arguments,
+  Block,
   Call,
   Primary,
   PrimaryType,
   PrimaryWithArguments,
+  ReturnStatement,
 } from "../../ast/type.ts";
+import { TestDataBase } from "./data.ts";
+import { Environment } from "../../eval/type.ts";
+import { UserFunction } from "../../eval/type.ts";
+import { Value, ValueType } from "../../eval/type.ts";
 
-type TestData = {
-  name: string;
-  program: string;
-  lines: number;
-  tokens: Token[];
+type TestData = TestDataBase & {
   ast: Call;
 };
 
@@ -24,6 +26,39 @@ export const callTests: TestData[] = [
       { type: TokenType.Identifier, value: "hello" },
     ],
     ast: new Primary(PrimaryType.Identifier, "hello"),
+    value: new Value(
+      ValueType.UserFunction,
+      new UserFunction(
+        [],
+        new Block([
+          new ReturnStatement(new Primary(PrimaryType.String, "hello")),
+        ]),
+      ),
+    ),
+    environmentBefore: new Environment().add(
+      "hello",
+      new Value(
+        ValueType.UserFunction,
+        new UserFunction(
+          [],
+          new Block([
+            new ReturnStatement(new Primary(PrimaryType.String, "hello")),
+          ]),
+        ),
+      ),
+    ),
+    environmentAfter: new Environment().add(
+      "hello",
+      new Value(
+        ValueType.UserFunction,
+        new UserFunction(
+          [],
+          new Block([
+            new ReturnStatement(new Primary(PrimaryType.String, "hello")),
+          ]),
+        ),
+      ),
+    ),
   },
   {
     name: "call without any arguments",
@@ -38,99 +73,132 @@ export const callTests: TestData[] = [
       new Primary(PrimaryType.Identifier, "hello"),
       [new Arguments([])],
     ),
-  },
-  {
-    name: "return function and call it",
-    program: `hello()()`,
-    lines: 1,
-    tokens: [
-      { type: TokenType.Identifier, value: "hello" },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.ParenRight },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.ParenRight },
-    ],
-    ast: new PrimaryWithArguments(
-      new Primary(PrimaryType.Identifier, "hello"),
-      [new Arguments([]), new Arguments([])],
+    value: new Value(ValueType.String, "hello"),
+    environmentBefore: new Environment().add(
+      "hello",
+      new Value(
+        ValueType.UserFunction,
+        new UserFunction(
+          [],
+          new Block([
+            new ReturnStatement(new Primary(PrimaryType.String, "hello")),
+          ]),
+        ),
+      ),
+    ),
+    environmentAfter: new Environment().add(
+      "hello",
+      new Value(
+        ValueType.UserFunction,
+        new UserFunction(
+          [],
+          new Block([
+            new ReturnStatement(new Primary(PrimaryType.String, "hello")),
+          ]),
+        ),
+      ),
     ),
   },
-  {
-    name: "call with 2 arguments",
-    program: `add(1, 2)`,
-    lines: 1,
-    tokens: [
-      { type: TokenType.Identifier, value: "add" },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.Number, value: 1 },
-      { type: TokenType.Comma },
-      { type: TokenType.Number, value: 2 },
-      { type: TokenType.ParenRight },
-    ],
-    ast: new PrimaryWithArguments(
-      new Primary(PrimaryType.Identifier, "add"),
-      [
-        new Arguments([
-          new Primary(PrimaryType.Number, 1),
-          new Primary(PrimaryType.Number, 2),
-        ]),
-      ],
-    ),
-  },
-  {
-    name: "curried function",
-    program: `add(1)(2)`,
-    lines: 1,
-    tokens: [
-      { type: TokenType.Identifier, value: "add" },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.Number, value: 1 },
-      { type: TokenType.ParenRight },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.Number, value: 2 },
-      { type: TokenType.ParenRight },
-    ],
-    ast: new PrimaryWithArguments(
-      new Primary(PrimaryType.Identifier, "add"),
-      [
-        new Arguments([
-          new Primary(PrimaryType.Number, 1),
-        ]),
-        new Arguments([
-          new Primary(PrimaryType.Number, 2),
-        ]),
-      ],
-    ),
-  },
-  {
-    name: "curried function 2",
-    program: `add(1, 2)(1, 2)`,
-    lines: 1,
-    tokens: [
-      { type: TokenType.Identifier, value: "add" },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.Number, value: 1 },
-      { type: TokenType.Comma },
-      { type: TokenType.Number, value: 2 },
-      { type: TokenType.ParenRight },
-      { type: TokenType.ParenLeft },
-      { type: TokenType.Number, value: 1 },
-      { type: TokenType.Comma },
-      { type: TokenType.Number, value: 2 },
-      { type: TokenType.ParenRight },
-    ],
-    ast: new PrimaryWithArguments(
-      new Primary(PrimaryType.Identifier, "add"),
-      [
-        new Arguments([
-          new Primary(PrimaryType.Number, 1),
-          new Primary(PrimaryType.Number, 2),
-        ]),
-        new Arguments([
-          new Primary(PrimaryType.Number, 1),
-          new Primary(PrimaryType.Number, 2),
-        ]),
-      ],
-    ),
-  },
+  // {
+  //   name: "call without any arguments",
+  //   program: `hello()`,
+  //   lines: 1,
+  //   tokens: [
+  //     { type: TokenType.Identifier, value: "hello" },
+  //     { type: TokenType.ParenLeft },
+  // },
+  // {
+  //   name: "return function and call it",
+  //   program: `hello()()`,
+  //   lines: 1,
+  //   tokens: [
+  //     { type: TokenType.Identifier, value: "hello" },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.ParenRight },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.ParenRight },
+  //   ],
+  //   ast: new PrimaryWithArguments(
+  //     new Primary(PrimaryType.Identifier, "hello"),
+  //     [new Arguments([]), new Arguments([])],
+  //   ),
+  // },
+  // {
+  //   name: "call with 2 arguments",
+  //   program: `add(1, 2)`,
+  //   lines: 1,
+  //   tokens: [
+  //     { type: TokenType.Identifier, value: "add" },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.Number, value: 1 },
+  //     { type: TokenType.Comma },
+  //     { type: TokenType.Number, value: 2 },
+  //     { type: TokenType.ParenRight },
+  //   ],
+  //   ast: new PrimaryWithArguments(
+  //     new Primary(PrimaryType.Identifier, "add"),
+  //     [
+  //       new Arguments([
+  //         new Primary(PrimaryType.Number, 1),
+  //         new Primary(PrimaryType.Number, 2),
+  //       ]),
+  //     ],
+  //   ),
+  // },
+  // {
+  //   name: "curried function",
+  //   program: `add(1)(2)`,
+  //   lines: 1,
+  //   tokens: [
+  //     { type: TokenType.Identifier, value: "add" },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.Number, value: 1 },
+  //     { type: TokenType.ParenRight },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.Number, value: 2 },
+  //     { type: TokenType.ParenRight },
+  //   ],
+  //   ast: new PrimaryWithArguments(
+  //     new Primary(PrimaryType.Identifier, "add"),
+  //     [
+  //       new Arguments([
+  //         new Primary(PrimaryType.Number, 1),
+  //       ]),
+  //       new Arguments([
+  //         new Primary(PrimaryType.Number, 2),
+  //       ]),
+  //     ],
+  //   ),
+  // },
+  // {
+  //   name: "curried function 2",
+  //   program: `add(1, 2)(1, 2)`,
+  //   lines: 1,
+  //   tokens: [
+  //     { type: TokenType.Identifier, value: "add" },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.Number, value: 1 },
+  //     { type: TokenType.Comma },
+  //     { type: TokenType.Number, value: 2 },
+  //     { type: TokenType.ParenRight },
+  //     { type: TokenType.ParenLeft },
+  //     { type: TokenType.Number, value: 1 },
+  //     { type: TokenType.Comma },
+  //     { type: TokenType.Number, value: 2 },
+  //     { type: TokenType.ParenRight },
+  //   ],
+  //   ast: new PrimaryWithArguments(
+  //     new Primary(PrimaryType.Identifier, "add"),
+  //     [
+  //       new Arguments([
+  //         new Primary(PrimaryType.Number, 1),
+  //         new Primary(PrimaryType.Number, 2),
+  //       ]),
+  //       new Arguments([
+  //         new Primary(PrimaryType.Number, 1),
+  //         new Primary(PrimaryType.Number, 2),
+  //       ]),
+  //     ],
+  //   ),
+  // },
 ];
